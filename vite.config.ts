c06681728d440305bdf2e386b04a55f351bf160c
jluce801 <jluce@uic.edu>
2025-11-11 21:28:48 -0600

    vita config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+      base: '/Snag/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
